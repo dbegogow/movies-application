017b23779e603b8eb8135235d63c7806ae2e818b
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-10 19:51:20 +0300

    Implement handling routes that do not exist

--- src/route-config.ts
+++ src/route-config.ts
@@ -1,4 +1,3 @@
-import LandingPage from './movies/LandingPage';
 import IndexGenres from './genres/IndexGenres';
 import CreateGenre from './genres/CreateGenre';
 import EditGenre from './genres/EditGenre';
@@ -11,10 +10,10 @@ import EditMovieTheater from './movieTheaters/EditMovieTheater';
 import CreateMovie from './movies/CreateMovie';
 import EditMovie from './movies/EditMovie';
 import FilterMovies from './movies/FilterMovies';
+import LandingPage from './movies/LandingPage';
+import RedirectToLandingPage from './utils/RedirectToLandingPage';
 
 const routes = [
-    { path: '/', component: LandingPage, exact: true },
-
     { path: '/genres', component: IndexGenres, exact: true },
     { path: '/genres/create', component: CreateGenre },
     { path: '/genres/edit/:id(\\d+)', component: EditGenre },
@@ -29,7 +28,10 @@ const routes = [
 
     { path: '/movies/create', component: CreateMovie },
     { path: '/movies/edit/:id(\\d+)', component: EditMovie },
-    { path: '/movies/filter', component: FilterMovies }
+    { path: '/movies/filter', component: FilterMovies },
+
+    { path: '/', component: LandingPage, exact: true },
+    { path: '*', component: RedirectToLandingPage }
 ];
 
 export default routes;
