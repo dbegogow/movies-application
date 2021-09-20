83c203e63a617bca0c32048da80448b63e968b16
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-20 11:39:28 +0300

    Fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
             </div>
             <footer className="bd-footer py-5 mt-5 bg-light">
                 <div className="container">
-                    Movies Application {new Date().getFullYear.toString()}
+                    Movies Application {new Date().getFullYear().toString()}
                 </div>
             </footer>
         </BrowserRouter>
