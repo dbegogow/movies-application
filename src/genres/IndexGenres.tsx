993060a1b0984b270625de214d642dbe678c602b
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-20 11:37:46 +0300

    Implement request to server with axios

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,6 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { genreDTO } from './genres.module';
 
 export default function IndexGenres() {
+    useEffect(() => {
+        axios
+            .get('https://localhost:5001/api/genres')
+            .then((response: AxiosResponse<genreDTO[]>) => {
+                console.log(response.data);
+            });
+    }, []);
+
     return (
         <>
             <h3>Genres</h3>
